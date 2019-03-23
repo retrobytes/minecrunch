949cb096565c38cdde9e0c026e03c42f43dcb04a
jon-proietti <Jon Proietti>
2019-03-22 22:23:34 -0500

    EP2

---
+++ src/minecrunch.libraries/minecrunch.generator/TerrainNoise.cs
@@ -0,0 +1,43 @@
+﻿using SharpNoise.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minecrunch.generator
+{
+    public class TerrainNoise
+    {
+        readonly WorldGenerator worldGenerator;
+        readonly WorldGeneratorSettings worldGenSettings;
+        readonly Module worldGenModule;
+        
+        const double WORLD_HORIZONTAL_STRETCH = 1 / 256;
+
+        private static readonly Lazy<TerrainNoise> lazy = new Lazy<TerrainNoise>(() => new TerrainNoise());
+
+        public static TerrainNoise Instance
+        {
+            get
+            {
+                return lazy.Value;
+            }
+        }
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        private TerrainNoise()
+        {
+            worldGenSettings = new WorldGeneratorSettings();
+            worldGenerator = new WorldGenerator(worldGenSettings);
+            worldGenModule = worldGenerator.CreatePlanetModule();
+        }
+
+        public double Terrain(double x, double z)
+        {
+            return worldGenModule.GetValue(x * WORLD_HORIZONTAL_STRETCH, 0, z * WORLD_HORIZONTAL_STRETCH);
+        }
+    }
+}
